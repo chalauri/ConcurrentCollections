c02ac980f2fd75ba03edfa6ba672fcccce3538a0
g.chalauri <g.chalauri@mia.gov.ge>
2017-03-29 13:26:22 +0400

    threadsafe navigable maps example

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,11 @@ import main.nonblocking_threadsafe_lists.AddTask;
 import main.nonblocking_threadsafe_lists.PollTask;
 import main.threadsafe_lists_with_delayed_elements.DEvent;
 import main.threadsafe_lists_with_delayed_elements.DTask;
+import main.threadsafe_navigable_maps.CTask;
+import main.threadsafe_navigable_maps.Contact;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.*;
 
 /**
@@ -21,7 +24,57 @@ public class Main {
         // blockingThreadsafeListsExample();
         // blockingThreadsafeListByPriorityExample();
         // threadsafeListsWithDelayedElementsExample();
+        threadsafeNavigableMapExample();
     }
+
+    private static void threadsafeNavigableMapExample() {
+        ConcurrentSkipListMap<String, Contact> map;
+        map = new ConcurrentSkipListMap<>();
+
+        Thread threads[] = new Thread[25];
+        int counter = 0;
+
+        for (char i = 'A'; i < 'Z'; i++) {
+            CTask task = new CTask(map, String.valueOf(i));
+            threads[counter] = new Thread(task);
+            threads[counter].start();
+            counter++;
+        }
+
+        for (int i = 0; i < 25; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.printf("Main: Size of the map: %d\n", map.size());
+        Map.Entry<String, Contact> element;
+        Contact contact;
+        element = map.firstEntry();
+        contact = element.getValue();
+        System.out.printf("Main: First Entry: %s: %s\n", contact.
+                getName(), contact.getPhone());
+
+        element = map.lastEntry();
+        contact = element.getValue();
+        System.out.printf("Main: Last Entry: %s: %s\n", contact.
+                getName(), contact.getPhone());
+
+
+        System.out.printf("Main: Submap from A1996 to B1002: \n");
+        ConcurrentNavigableMap<String, Contact> submap = map.
+                subMap("A1996", "B1002");
+        do {
+            element = submap.pollFirstEntry();
+            if (element != null) {
+                contact = element.getValue();
+                System.out.printf("%s: %s\n", contact.getName(), contact.
+                        getPhone());
+            }
+        } while (element != null);
+}
 
     private static void threadsafeListsWithDelayedElementsExample() throws InterruptedException {
         DelayQueue<DEvent> queue = new DelayQueue<>();
