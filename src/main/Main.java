43631b2a520cbf95c6b526e554bd8eedcba11760
g.chalauri <g.chalauri@mia.gov.ge>
2017-03-29 11:53:01 +0400

    blocking thread-safe lists example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,21 @@
 package main;
 
+import main.blocking_threadsafe_lists.Client;
 import main.nonblocking_threadsafe_lists.AddTask;
 import main.nonblocking_threadsafe_lists.PollTask;
 
+import java.util.Date;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by G.Chalauri on 03/29/17.
  */
 public class Main {
-    public static void main(String[] args) {
-        nonblockingThreadsafeListsExample();
+    public static void main(String[] args) throws InterruptedException {
+       // nonblockingThreadsafeListsExample();
+        blockingThreadsafeListsExample();
     }
 
     //non-blocking thread-safe lists example
@@ -36,17 +41,17 @@ public class Main {
             }
         }
 
-        System.out.printf("Main: Size of the List: %d\n",list.size());
+        System.out.printf("Main: Size of the List: %d\n", list.size());
 
 
-        for (int i=0; i< threads.length; i++){
-            PollTask task=new PollTask(list);
-            threads[i]=new Thread(task);
+        for (int i = 0; i < threads.length; i++) {
+            PollTask task = new PollTask(list);
+            threads[i] = new Thread(task);
             threads[i].start();
         }
-        System.out.printf("Main: %d PollTask threads have been launched\n",threads.length);
+        System.out.printf("Main: %d PollTask threads have been launched\n", threads.length);
 
-        for (int i=0; i<threads.length; i++) {
+        for (int i = 0; i < threads.length; i++) {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
@@ -54,7 +59,26 @@ public class Main {
             }
         }
 
-        System.out.printf("Main: Size of the List: %d\n",list.size());
+        System.out.printf("Main: Size of the List: %d\n", list.size());
 
+    }
+
+    //blocking thread-safe lists example
+    private static void blockingThreadsafeListsExample() throws InterruptedException {
+        LinkedBlockingDeque<String> list = new LinkedBlockingDeque<>(3);
+
+        Client client = new Client(list);
+        Thread thread = new Thread(client);
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                String request = list.take();
+                System.out.printf("Main: Request: %s at %s. Size: %d\n", request, new Date(), list.size());
+            }
+            TimeUnit.MILLISECONDS.sleep(300);
+        }
+
+        System.out.printf("Main: End of the program.\n");
     }
 }
